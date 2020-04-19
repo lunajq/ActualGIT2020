5d35272abcb68434474e1e696b61cf75086c4729
anuheatao <62665681+anuheatao@users.noreply.github.com>
2020-04-18 20:30:44 -0700

    extra files

--- HomeViewController.swift
+++ HomeViewController.swift
@@ -9,13 +9,32 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-{@IBAction func PhysicalHealthButtonPressed(_ sender: Any) {
+
+    @IBAction func HygieneButtonPressed(_ sender: Any) {
+        self.performSegue(withIdentifier: "HygieneSegue", sender: self)
     }
-}
+    
+    
+    @IBAction func SexualityButtonPressed(_ sender: Any) {
+        print("button pressed")
+self.performSegue(withIdentifier: "SexualitySegue", sender: self)
+    }
+    
+    @IBAction func BodyImageButtonPressed(_ sender: Any) {
+        self.performSegue(withIdentifier: "BodyImageSegue", sender: self)
+    }
+    @IBAction func MentalHealthButtonPressed(_ sender: Any) {
+          self.performSegue(withIdentifier: "MentalHealthSegue", sender: self)
+    
+    }
+    
+    @IBAction func unwindToHomeViewController(Segue: UIStoryboardSegue) {
+           
+       }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
         print("view has loaded")
     }
 
